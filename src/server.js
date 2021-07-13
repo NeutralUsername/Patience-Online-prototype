6e5f2b8a3512343881e032b53157e462d741ea55
c <chris@hribhrib.at>
2021-07-13 16:27:37 +0200

    fix bug that was caused running the server on a parent directory; add '..' to index.html and 'path'-library to server

--- src/server.js
+++ src/server.js
@@ -6,12 +6,15 @@ const app = express();
 const server = http.createServer(app);
 const io = new socketio.Server(server);
 
+var path = require('path');
+
+
 app.get('/script.js', (req, res) => {
     res.sendFile(__dirname + '/script.js');
 });
 
 app.get('/node_modules/socket.io/client-dist/socket.io.js', (req, res) => {
-    res.sendFile(__dirname + '/node_modules/socket.io/client-dist/socket.io.js');
+    res.sendFile(path.resolve('node_modules/socket.io/client-dist/socket.io.js'));
 });
 
 app.get('/', (req, res) => {
