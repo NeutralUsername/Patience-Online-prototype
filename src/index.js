a0380644793e1502a91b747eded584de03e6c28c
stu17283 <stu17283@spengergasse.at>
2021-07-19 16:58:59 +0200

    started implementing front-end of solitaire with react

--- src/index.js
+++ src/index.js
@@ -23,6 +23,7 @@ class Stack {
         this.stack = cards;
     }
 }
+
 class Deck  {
     constructor() {
         this.cards =  freshDeck();
@@ -52,147 +53,19 @@ function shuffle(deck) {
     return deck;
 }
 
-class Board extends React.Component {
+
+class SolitaireApp extends React.Component {
     render() {
         return (
-            <div className="game">
-                <div className="game-board">
-                    <TicTacToeBoard />
-                </div>
-                <div className="game-info">
-                    <div>{/* status */}</div>
-                    <ol>{/* TODO */}</ol>
-                </div>
-            </div>
+            <input className="Player">
+
+            </input>
         );
     }
 }
-class Solitaire extends React.Component {
-    render() {
-        return (
-            <div className="game">
-                <div className="game-board">
-                    <TicTacToeBoard />
-                </div>
-                <div className="game-info">
-                    <div>{/* status */}</div>
-                    <ol>{/* TODO */}</ol>
-                </div>
-            </div>
-        );
-    }
-}
-
-//======================================================================================================================
-
-
-
-function Square(props) {
-    return (
-        <button className="square" onClick={props.onClick}>
-            {props.value}
-        </button>
-    );
-}
-
-class TicTacToeBoard extends React.Component
-{
-    constructor(props)
-    {
-        super(props);
-        this.state =
-            {
-                squares: Array(9).fill(null),
-                xIsNext: true,
-            };
-    }
-    handleClick(i) {
-        const squares = this.state.squares.slice();
-        if (calculateWinner(squares) || squares[i]) {
-            return;
-        }
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({
-            squares: squares,
-            xIsNext: !this.state.xIsNext,
-        });
-    }
-    renderSquare(i)
-    {
-        return <Square value={this.state.squares[i]}  onClick={() => this.handleClick(i)} />;
-    }
-    render()
-    {
-        const winner = calculateWinner(this.state.squares);
-        let status;
-        if (winner) {
-            status = 'Winner: ' + winner;
-        } else {
-            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-        }
-        return (
-            <div>
-                <div className="status">{status}</div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
-            </div>
-        );
-    }
-}
-
-class TicTacToe extends React.Component {
-    render() {
-        return (
-            <div className="game">
-                <div className="game-board">
-                    <TicTacToeBoard />
-                </div>
-                <div className="game-info">
-                    <div>{/* status */}</div>
-                    <ol>{/* TODO */}</ol>
-                </div>
-            </div>
-        );
-    }
-}
-
 // ========================================
 
 ReactDOM.render(
-    <TicTacToe />,
+    <SolitaireApp />,
     document.getElementById('root')
 );
-
-
-function calculateWinner(squares) {
-    const lines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-        const [a, b, c] = lines[i];
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a];
-        }
-    }
-    return null;
-}
