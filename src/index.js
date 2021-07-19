26f7d0ffd32576d9206d064ae6efb0c9a5b01c84
stu17283 <stu17283@spengergasse.at>
2021-07-19 15:46:51 +0200

    started implementing front-end of solitaire with react

--- src/index.js
+++ src/index.js
@@ -2,78 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Square(props) {
-    return (
-        <button className="square" onClick={props.onClick}>
-            {props.value}
-        </button>
-    );
+class Card {
+   constructor(suit, value) {
+       this.suit = suit;
+       this.value = value;
+   }
 }
-
-class Board extends React.Component
-{
-    constructor(props)
-    {
-        super(props);
-        this.state =
-            {
-                squares: Array(9).fill(null),
-                xIsNext: true,
-            };
-    }
-    handleClick(i) {
-        const squares = this.state.squares.slice();
-        if (calculateWinner(squares) || squares[i]) {
-            return;
-        }
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({
-            squares: squares,
-            xIsNext: !this.state.xIsNext,
+function freshDeck() {
+    const Suits = ["♠", "♥", "♦", "♣"];
+    const Values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+    return Suits.map(suit => {
+        return Values.map(value => {
+            return new Card(suit, value)
         });
-    }
-    renderSquare(i)
-    {
-        return <Square value={this.state.squares[i]}  onClick={() => this.handleClick(i)} />;
-    }
-    render()
-    {
-        const winner = calculateWinner(this.state.squares);
-        let status;
-        if (winner) {
-            status = 'Winner: ' + winner;
-        } else {
-            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-        }
-        return (
-            <div>
-                <div className="status">{status}</div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
-            </div>
-        );
-    }
+    });
 }
 
-class Game extends React.Component {
+class Deck  {
+    constructor() {
+        this.cards =  freshDeck();
+    }
+
+}
+
+class Solitaire extends React.Component {
     render() {
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board />
+                    <TicTacToeBoard/>
+                    <Deck/>
                 </div>
                 <div className="game-info">
                     <div>{/* status */}</div>
@@ -84,10 +42,10 @@ class Game extends React.Component {
     }
 }
 
-// ========================================
+// =====================================================================================================================
 
 ReactDOM.render(
-    <Game />,
+    <TicTacToe />,
     document.getElementById('root')
 );
 
@@ -110,4 +68,89 @@ function calculateWinner(squares) {
         }
     }
     return null;
+
+    // =================================================================================================================
+
+    function Square(props) {
+        return (
+            <button className="square" onClick={props.onClick}>
+                {props.value}
+            </button>
+        );
+    }
+
+    class TicTacToeBoard extends React.Component {
+        constructor(props) {
+            super(props);
+            this.state =
+                {
+                    squares: Array(9).fill(null),
+                    xIsNext: true,
+                };
+        }
+
+        handleClick(i) {
+            const squares = this.state.squares.slice();
+            if (calculateWinner(squares) || squares[i]) {
+                return;
+            }
+            squares[i] = this.state.xIsNext ? 'X' : 'O';
+            this.setState({
+                squares: squares,
+                xIsNext: !this.state.xIsNext,
+            });
+        }
+
+        renderSquare(i) {
+            return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)}/>;
+        }
+
+        render() {
+            const winner = calculateWinner(this.state.squares);
+            let status;
+            if (winner) {
+                status = 'Winner: ' + winner;
+            } else {
+                status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+            }
+            return (
+                <div>
+                    <div className="status">{status}</div>
+                    <div className="board-row">
+                        {this.renderSquare(0)}
+                        {this.renderSquare(1)}
+                        {this.renderSquare(2)}
+                    </div>
+                    <div className="board-row">
+                        {this.renderSquare(3)}
+                        {this.renderSquare(4)}
+                        {this.renderSquare(5)}
+                    </div>
+                    <div className="board-row">
+                        {this.renderSquare(6)}
+                        {this.renderSquare(7)}
+                        {this.renderSquare(8)}
+                    </div>
+                </div>
+            );
+        }
+    }
+
+    class TicTacToe extends React.Component {
+        render() {
+            return (
+                <div className="game">
+                    <div className="game-board">
+                        <TicTacToeBoard/>
+                        <Deck/>
+                    </div>
+                    <div className="game-info">
+                        <div>{/* status */}</div>
+                        <ol>{/* TODO */}</ol>
+                    </div>
+                </div>
+            );
+        }
+    }
+
 }
---
+++ src/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const http = require('http');
+const socketio= require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new socketio.Server(server);
+
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/script.js', (req, res) => {
+    res.sendFile(__dirname + '/script.js');
+});
+
+
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+});
+
+server.listen(4200, () => {
+    console.log("listening on port 4200");
+});
