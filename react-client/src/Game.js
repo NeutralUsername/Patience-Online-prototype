1af84e80d478dddb19a42f8670351a6c262e2929
NeutralUsername <39095721+NeutralUsername@users.noreply.github.com>
2021-08-27 19:14:24 +0200

    removed beatiful dnd and added initial react-dnd support. changed most options compoenents to functions since they dont have a state

--- react-client/src/Game.js
+++ react-client/src/Game.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import socketIOClient from 'socket.io-client';
 import ReactDOM from 'react-dom';
-
+import { useDrag, DndProvider, useDrop  } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
 
 export default class Game extends React.Component{
     constructor(props) {
@@ -35,165 +36,164 @@ export default class Game extends React.Component{
 
     render(){
         return (
-            <div className="game">
-                <DragDropContext onDragEnd = {this.handleOnDragEnd} >
-                    <Stack 
-                        cards = {this.state.stacks.playermalus} 
-                        name ="playermalus" 
-                        type ="sequence" 
-                    ></Stack>
-                    <Stack 
-                        cards = {this.state.stacks.playerstock} 
-                        name = "playerstock" 
-                        type ="pile" 
-                    ></Stack>
-                    <Stack 
-                        cards = {this.state.stacks.playerwaste} 
-                        name = "playerwaste" 
-                        type ="pile" 
-                    ></Stack>
-                    <Stack 
-                        cards = {this.state.stacks.playertableau0} 
-                        name = "playertableau0" 
-                        type ="pile" 
-                    ></Stack>
-                    <Stack 
-                        cards = {this.state.stacks.playerfoundation0} 
-                        name = "playerfoundation0" 
-                        type ="sequence" 
-                    ></Stack>
-                    <Stack 
-                        cards = {this.state.stacks.opponentfoundation0} 
-                        name = "opponentfoundation0" 
-                        type ="sequence" 
-                    ></Stack>
-                    <Stack 
-                        cards = {this.state.stacks.opponenttableau0} 
-                        name = "opponenttableau0" 
-                        type ="pile" 
-                    ></Stack>
-                    <Stack 
-                        cards = {this.state.stacks.playertableau1} 
-                        name = "playertableau1" 
-                        type ="pile" 
-                    ></Stack>
-                    <Stack 
-                        cards = {this.state.stacks.playerfoundation1} 
-                        name = "playerfoundation1" 
-                        type ="sequence" 
-                    ></Stack>
-                    <Stack 
-                        cards = {this.state.stacks.opponentfoundation1} 
-                        name = "opponentfoundation1" 
-                        type ="sequence" 
-                    ></Stack>
-                    <Stack 
-                        cards = {this.state.stacks.opponenttableau1} 
-                        name = "opponenttableau1" 
-                        type ="pile" 
-                    ></Stack>
-                    <Stack 
-                        cards = {this.state.stacks.playertableau2} 
-                        name = "playertableau2" 
-                        type ="pile" 
-                    ></Stack>
-                    <Stack 
-                        cards = {this.state.stacks.playerfoundation2} 
-                        name = "playerfoundation2" 
-                        type ="sequence" 
-                    ></Stack>
-                    <Stack 
-                        cards = {this.state.stacks.opponentfoundation2} 
-                        name = "opponentfoundation2" 
-                        type ="sequence" 
-                    ></Stack>
-                    <Stack 
-                        cards = {this.state.stacks.opponenttableau2} 
-                        name = "opponenttableau2" 
-                        type ="pile" 
-                    ></Stack> 
-                    <Stack 
-                        cards = {this.state.stacks.playertableau3} 
-                        name = "playertableau3" 
-                        type ="pile" 
-                    ></Stack>
-                    <Stack 
-                        cards = {this.state.stacks.playerfoundation3} 
-                        name = "playerfoundation3" 
-                        type ="sequence" 
-                    ></Stack>
-                    <Stack 
-                        cards = {this.state.stacks.opponentfoundation3} 
-                        name = "opponentfoundation3" 
-                        type ="sequence" 
-                    ></Stack>
-                    <Stack 
-                        cards = {this.state.stacks.opponenttableau3} 
-                        name = "opponenttableau3" 
-                        type ="pile" 
-                    ></Stack>
-                    <Stack 
-                        cards = {this.state.stacks.opponentwaste} 
-                        name = "opponentwaste" 
-                        type ="pile" 
-                    ></Stack>
-                    <Stack 
-                        cards = {this.state.stacks.opponentstock} 
-                        name = "opponentstock" 
-                        type ="pile" 
-                    ></Stack>
-                     <Stack 
-                        cards = {this.state.stacks.opponentmalus} 
-                        name = "opponentmalus" 
-                        type ="sequence" 
-                    ></Stack>
-                </DragDropContext>
-            </div>
+            <DndProvider backend={HTML5Backend}>
+                <div className="game">
+                        <Stack 
+                            cards = {this.state.stacks.playermalus} 
+                            name ="playermalus" 
+                            type ="sequence" 
+                        ></Stack>
+                        <Stack 
+                            cards = {this.state.stacks.playerstock} 
+                            name = "playerstock" 
+                            type ="pile" 
+                        ></Stack>
+                        <Stack 
+                            cards = {this.state.stacks.playerwaste} 
+                            name = "playerwaste" 
+                            type ="pile" 
+                        ></Stack>
+                        <Stack 
+                            cards = {this.state.stacks.playertableau0} 
+                            name = "playertableau0" 
+                            type ="pile" 
+                        ></Stack>
+                        <Stack 
+                            cards = {this.state.stacks.playerfoundation0} 
+                            name = "playerfoundation0" 
+                            type ="sequence" 
+                        ></Stack>
+                        <Stack 
+                            cards = {this.state.stacks.opponentfoundation0} 
+                            name = "opponentfoundation0" 
+                            type ="sequence" 
+                        ></Stack>
+                        <Stack 
+                            cards = {this.state.stacks.opponenttableau0} 
+                            name = "opponenttableau0" 
+                            type ="pile" 
+                        ></Stack>
+                        <Stack 
+                            cards = {this.state.stacks.playertableau1} 
+                            name = "playertableau1" 
+                            type ="pile" 
+                        ></Stack>
+                        <Stack 
+                            cards = {this.state.stacks.playerfoundation1} 
+                            name = "playerfoundation1" 
+                            type ="sequence" 
+                        ></Stack>
+                        <Stack 
+                            cards = {this.state.stacks.opponentfoundation1} 
+                            name = "opponentfoundation1" 
+                            type ="sequence" 
+                        ></Stack>
+                        <Stack 
+                            cards = {this.state.stacks.opponenttableau1} 
+                            name = "opponenttableau1" 
+                            type ="pile" 
+                        ></Stack>
+                        <Stack 
+                            cards = {this.state.stacks.playertableau2} 
+                            name = "playertableau2" 
+                            type ="pile" 
+                        ></Stack>
+                        <Stack 
+                            cards = {this.state.stacks.playerfoundation2} 
+                            name = "playerfoundation2" 
+                            type ="sequence" 
+                        ></Stack>
+                        <Stack 
+                            cards = {this.state.stacks.opponentfoundation2} 
+                            name = "opponentfoundation2" 
+                            type ="sequence" 
+                        ></Stack>
+                        <Stack 
+                            cards = {this.state.stacks.opponenttableau2} 
+                            name = "opponenttableau2" 
+                            type ="pile" 
+                        ></Stack> 
+                        <Stack 
+                            cards = {this.state.stacks.playertableau3} 
+                            name = "playertableau3" 
+                            type ="pile" 
+                        ></Stack>
+                        <Stack 
+                            cards = {this.state.stacks.playerfoundation3} 
+                            name = "playerfoundation3" 
+                            type ="sequence" 
+                        ></Stack>
+                        <Stack 
+                            cards = {this.state.stacks.opponentfoundation3} 
+                            name = "opponentfoundation3" 
+                            type ="sequence" 
+                        ></Stack>
+                        <Stack 
+                            cards = {this.state.stacks.opponenttableau3} 
+                            name = "opponenttableau3" 
+                            type ="pile" 
+                        ></Stack>
+                        <Stack 
+                            cards = {this.state.stacks.opponentwaste} 
+                            name = "opponentwaste" 
+                            type ="pile" 
+                        ></Stack>
+                        <Stack 
+                            cards = {this.state.stacks.opponentstock} 
+                            name = "opponentstock" 
+                            type ="pile" 
+                        ></Stack>
+                        <Stack 
+                            cards = {this.state.stacks.opponentmalus} 
+                            name = "opponentmalus" 
+                            type ="sequence" 
+                        ></Stack>
+                </div>
+            </DndProvider>
         )
     }
 } 
 
-class Stack extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+function Stack (props) {
+    const [{  }, drop] = useDrop(() => ({
+        accept: "card",
+        drop: () => {},
+        collect: monitor => ({
+          isOver: !!monitor.isOver(),
+        }),
+      }), [])
     
-    render () {
+  
         return (
-            <div className={"stack"} >
-                
-                <Droppable droppableId = {this.props.name}  type="CARDS" direction={'horizontal'} >
-                    {(provided) => ( 
-                        <ul className ={this.props.type+" "+this.props.name} ref = {provided.innerRef} {...provided.droppableProps} > 
-                        
-                        {this.props.cards ? Object.keys(this.props.cards).map( (card, index) => 
-                            <Draggable key = {String(this.props.cards[card].cardid)} draggableId= {String(this.props.cards[card].cardid)} index={index} > 
-                                {(provided) => (
-                                    <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} > 
-                                        <Card 
-                                            cardid = {this.props.cards[card].cardid}
-                                            faceup = {this.props.cards[card].faceup}
-                                            color = {this.props.cards[card].color} 
-                                            suit = {this.props.cards[card].suit} 
-                                            value = {this.props.cards[card].value}
-                                            stack = {this.props.name}
-                                        ></Card>
-                                    </div>
-                                )}
-                            </Draggable>
-                            ):''}
-                            {provided.placeholder}
-                        </ul>
-                    )}
-                </Droppable>
+            <div  ref={drop} className={"stack"} >
+                <ul className ={props.type+" "+props.name}  > 
+                    {props.cards ? Object.keys(props.cards).map( (card, index) => 
+                    <div  key = {String(props.cards[card].cardid)}> 
+                        <Card 
+                            cardid = {props.cards[card].cardid}
+                            faceup = {props.cards[card].faceup}
+                            color = {props.cards[card].color} 
+                            suit = {props.cards[card].suit} 
+                            value = {props.cards[card].value}
+                            stack = {props.name}
+                        ></Card>
+                    </div>
+                    ):''}
+                </ul>
             </div>
         )
     }
-}
+
 
 function Card (props) {
+    const [{ isDragging }, drag] = useDrag(() => ({
+        type: "card",
+        collect: (monitor) => ({
+          isDragging: !!monitor.isDragging()
+        })
+      }))
     return (
-        <div className = {'card '+"cards-"+ props.stack+' '+ props.color +' '+ (props.faceup ? 'faceup' : 'facedown')+ (props.faceup ? ' '+props.suit : '') +(props.faceup ? ' '+ props.value : '')} >
+        <div ref={drag}  className = {'card '+"cards-"+ props.stack+' '+ props.color +' '+ (props.faceup ? 'faceup' : 'facedown')+ (props.faceup ? ' '+props.suit : '') +(props.faceup ? ' '+ props.value : '')} >
             {props.suit} {props.value}
         </div>
     )
