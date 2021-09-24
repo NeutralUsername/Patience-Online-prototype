6ee119baa5e8c33fba8d807c320ac965909ef1ee
NeutralUsername <39095721+NeutralUsername@users.noreply.github.com>
2021-09-25 01:47:06 +0200

    removed newely added gamerules because they fail to make the game more interesting. keeping the rigged card dealing for now

--- nodejs-server/db.js
+++ nodejs-server/db.js
@@ -143,25 +143,21 @@ function insertActions(gameid, redDeck, blackDeck, malusSize, tableauSize, timeP
     var reshufflecounter = 0
     for(var player = 0; player < 2 ; player++) {
       for(var malussizeCounter = 0 ; malussizeCounter < malusSize; malussizeCounter++) {
-      
-        while ( player === 0 ?  redDeck[redDeck.length-1].value === "1" : blackDeck[blackDeck.length-1].value === "1" ) {
-          console.log(reshufflecounter++)
-          player === 0 ? redDeck = shuffle(redDeck) : blackDeck = shuffle(blackDeck)
-        } 
 
-        if (malussizeCounter < 11)
-          while ( player === 0 ?  redDeck[redDeck.length-1].value < 7 : blackDeck[blackDeck.length-1].value < 7 ) {
+        if (malussizeCounter < 10)
+          while ( player === 0 ?  redDeck[redDeck.length-1].value < 6 : blackDeck[blackDeck.length-1].value < 7 ) {
             console.log(reshufflecounter++)
             player === 0 ? redDeck = shuffle(redDeck) : blackDeck = shuffle(blackDeck)
         } 
-        
-        if (malussizeCounter > 10)
-        while ( player === 0 ?  redDeck[redDeck.length-1].value > 9 : blackDeck[blackDeck.length-1].value > 9 ) {
-          console.log(reshufflecounter++)
-          player === 0 ? redDeck = shuffle(redDeck) : blackDeck = shuffle(blackDeck)
-        } 
+    
+        if (malussizeCounter > 9)
+          while ( player === 0 ?  redDeck[redDeck.length-1].value > 7 ||redDeck[redDeck.length-1].value === "1" : blackDeck[blackDeck.length-1].value > 7 || blackDeck[blackDeck.length-1].value ==="1" ) {
+            console.log(reshufflecounter++)
+            player === 0 ? redDeck = shuffle(redDeck) : blackDeck = shuffle(blackDeck)
+          } 
       
         var card = player === 0 ? redDeck.pop(): blackDeck.pop() ;
+
         if(malussizeCounter === malusSize-1)
           card.faceup = true
         stacks[((player === 0) ? ('redmalus') : ('blackmalus'))].cards.push(card)
--- react-client/src/Game.js
+++ react-client/src/Game.js
@@ -471,9 +471,9 @@ function Stack (props) {
     function legalMove(movingCard, UppermostCard) {
         if( ! (movingCard.stackname.includes("tableau") || movingCard.stackname.includes("foundation") || movingCard.stackname === GameContext.playercolor+"stock" || movingCard.stackname === GameContext.playercolor+"malus") ) return false
       
-        if( !movingCard.stackname.includes("tableau") && UppermostCard.stackname.includes('foundation') && movingCard.value != 1) return false
-        if( !movingCard.stackname.includes("tableau") && UppermostCard.stackname === GameContext.opponentcolor+"waste") return false
-        if( !movingCard.stackname.includes("tableau") && UppermostCard.stackname === GameContext.opponentcolor+"malus") return false
+        // if( !movingCard.stackname.includes("tableau") && UppermostCard.stackname.includes('foundation') ) return false
+        // if( !movingCard.stackname.includes("tableau") && UppermostCard.stackname === GameContext.opponentcolor+"waste") return false
+        // if( !movingCard.stackname.includes("tableau") && UppermostCard.stackname === GameContext.opponentcolor+"malus") return false
      
         if(GameContext.stockflipped && movingCard.stackname != GameContext.playercolor+"stock" && UppermostCard.stackname != GameContext.playercolor+"waste") return false
         if(movingCard.stackname.includes('foundation') && (UppermostCard.stackname === GameContext.opponentcolor+"malus" || UppermostCard.stackname === GameContext.opponentcolor+"waste")) return false
